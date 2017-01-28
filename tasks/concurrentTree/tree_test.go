afbe7a06647e2b01c352a9dfffa14f1d17156eeb
Daniel Justice <danieljustice09@gmail.com>
2017-01-27 19:59:44 -0500

    Get basic test to work succesfully

--- tasks/concurrentTree/tree_test.go
+++ tasks/concurrentTree/tree_test.go
@@ -3,6 +3,8 @@ package concurrentTree
 import(
 	//"fmt"
 	"testing"
+	"context"
+	//"time"
 	//"log"
 )
 
@@ -20,5 +22,101 @@ func TestNewTree(t *testing.T){
 	//myTree.NewNode("BB", "B", nil)
 	for k := range myTree.Contents{
 		t.Log(k)
+	}
+}
+//is in tree_tester package, so this should not play with concurrentTree package
+
+
+type (
+
+
+	Walkable interface {
+		NewNode(name, parent string, contents Visitable)
+		Walk()
+	}
+
+	mockTreeNode struct {
+		to   chan string
+		data string
+	}
+)
+
+func (m *mockTreeNode) Visit(cont context.Context) (context.Context, error) {
+	//m.to is supposed to be a receive only type? but we are sending to it?
+	m.to <- m.data
+	//need to return a context? and an error?
+	return cont, nil
+}
+
+func newMockTreeNode(to chan string, data string) *mockTreeNode {
+	return &mockTreeNode{to, data}
+}
+
+func TestWalker(t *testing.T) {
+	const (
+		grandfather = "Grandfather"
+		father      = "Father"
+		mother      = "Mother"
+		son         = "son"
+		daughter    = "daughter"
+	)
+
+	var (
+		tree = NewTree()
+		output          = make(chan string, 5)
+		grandfatherNode = newMockTreeNode(output, "Grandfather")
+		fatherNode      = newMockTreeNode(output, "Father")
+		motherNode      = newMockTreeNode(output, "Mother")
+		sonNode         = newMockTreeNode(output, "Son")
+		daughterNode    = newMockTreeNode(output, "Daughter")
+	)
+
+	tree.NewNode(grandfather, "Root", grandfatherNode)
+	tree.NewNode(father, grandfather, fatherNode)
+	tree.NewNode(mother, grandfather, motherNode)
+	tree.NewNode(son, mother, sonNode)
+	tree.NewNode(daughter, mother, daughterNode)
+	//	close(output)
+
+	tree.Walk()
+	close(output)
+
+	checkbox := map[string]bool{
+		grandfather: false,
+		father:      false,
+		mother:      false,
+		son:         false,
+		daughter:    false,
+	}
+
+	for node := range output {
+		checkbox[node] = true
+		switch node {
+		case grandfather:
+			if checkbox[mother] || checkbox[father] ||
+				checkbox[son] || checkbox[daughter] {
+				t.Fail()
+			}
+		case father:
+			if !checkbox[grandfather] {
+				t.Fail()
+			}
+		case mother:
+			if !checkbox[grandfather] {
+				t.Fail()
+			}
+			if checkbox[son] || checkbox[daughter] {
+				t.Fail()
+			}
+		case son:
+			if !(checkbox[mother] && checkbox[grandfather]) {
+				t.Fail()
+			}
+
+		case daughter:
+			if !(checkbox[mother] && checkbox[grandfather]) {
+				t.Fail()
+			}	
+		}
 	}
 }
