d740a026c658a49cc6d626bf66d12fc93e779cbb
Robbie McKinstry <rem89@pitt.edu>
2017-03-29 18:41:46 -0400

    Refactor Checkstyle step

--- checkstyle.go
+++ checkstyle.go
@@ -10,58 +10,112 @@ import (
 )
 
 type checkstyleStep struct {
-	srcDir     string
-	configPath string
+	srcDir    string
+	jarLoc    string
+	outputLoc string
+	checkLoc  string
+	pipeline.StepContext
 }
 
 const (
-	checkstyleJar = "/checkstyle-7.6.1.jar"
-	checkstyleOut = "/checkstyle_output.txt"
-	checkPath     = "/checks.xml"
+	DefaultCheckstyleJarLoc    = "/checkstyle-7.6.1.jar"
+	DefaultCheckstyleOutputLoc = "/checkstyle_output.txt"
+	DefaultCheckstyleConfigLoc = "/checks.xml"
 
-	// TODO change this to use checkstyle instead
-	// TODO do i add the text output not to a file but to stdout and store it in the ctx?
 	cmdTmplCheckstyle = "java -jar %s -c %s -o %s %s"
 )
 
-func (checkstyle *checkstyleStep) Exec(request *pipeline.Request) *pipeline.Result {
+func NewCheckstyleStep(srcDir, jarLoc, outputLoc, checkLoc string) pipeline.Step {
+	return &checkstyleStep{
+		srcDir:    srcDir,
+		jarLoc:    jarLoc,
+		outputLoc: outputLoc,
+		checkLoc:  checkLoc,
+	}
+}
+
+func (checkstyle *checkstyleStep) Cancel() error {
+	checkstyle.Status("Cancel")
+	return nil
+}
+
+func (checkstyle *checkstyleStep) init(request *pipeline.Request) error {
+
+	if err := checkstyle.setSrcDir(request); err != nil {
+		return err
+	}
+
+	if checkstyle.jarLoc == "" {
+		checkstyle.jarLoc = DefaultCheckstyleJarLoc
+	}
+
+	if checkstyle.srcDir == "" {
+		checkstyle.srcDir = DefaultSrcDir
+	}
+
+	if checkstyle.outputLoc == "" {
+		checkstyle.outputLoc = DefaultCheckstyleOutputLoc
+	}
+
+	if checkstyle.checkLoc == "" {
+		checkstyle.checkLoc = DefaultCheckstyleConfigLoc
+	}
+
+	return nil
+}
+
+func (checkstyle *checkstyleStep) setSrcDir(request *pipeline.Request) error {
 
 	srcDirIntf, ok := request.KeyVal["src"]
 	if !ok {
-		return &pipeline.Result{Error: errors.New("No source directory set.")}
+		return errors.New("No source directory set.")
 	}
 
 	srcDir, ok := srcDirIntf.(string)
 	if !ok {
-		return &pipeline.Result{Error: errors.New("Source directory is not a string")}
+		return errors.New("Source directory is not a string")
 	}
 	checkstyle.srcDir = srcDir
+	return nil
+}
+
+func (checkstyle *checkstyleStep) launchCmd() (string, error) {
 
 	cmd := checkstyle.Cmd()
-	out, err := cmd.Output()
+	_, err := cmd.Output()
 	if err != nil {
-		return &pipeline.Result{Error: err}
+		return "", err
 	}
-	stdout := string(out)
-	request.KeyVal["checkpathStdout"] = stdout
 
-	contents, err := ioutil.ReadFile(checkstyleOut)
-	if err != nil {
+	contents, err := ioutil.ReadFile(checkstyle.outputLoc)
+	return string(contents), err
+}
+
+func (checkstyle *checkstyleStep) Exec(request *pipeline.Request) *pipeline.Result {
+
+	// Ensure all data is set
+	if err := checkstyle.init(request); err != nil {
 		return &pipeline.Result{Error: err}
 	}
-	request.KeyVal["checkstyle"] = string(contents)
+
+	// Now, launch the command
+	contents, err := checkstyle.launchCmd()
+	request.KeyVal["checkstyle"] = contents
 
 	return &pipeline.Result{
-		Error:  nil,
+		Error:  err,
 		KeyVal: request.KeyVal,
 	}
 }
 
 func (checkstyle *checkstyleStep) Cmd() *exec.Cmd {
-	cmd := tmplCmdCheckstyle(checkstyleJar, checkPath, checkstyleOut, checkstyle.srcDir)
-	return exec.Command("bash", "-c", cmd)
-}
+	cmd := fmt.Sprintf(
+		cmdTmplCheckstyle,
+		checkstyle.jarLoc,
+		checkstyle.checkLoc,
+		checkstyle.outputLoc,
+		checkstyle.srcDir,
+	)
 
-func tmplCmdCheckstyle(jarPath, checkPath, outputPath, srcPath string) string {
-	return fmt.Sprintf(cmdTmplCheckstyle, jarPath, checkPath, outputPath, srcPath)
+	return exec.Command("bash", "-c", cmd)
 }
